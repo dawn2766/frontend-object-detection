88f7ff928ca466d58462edae945f667286128e16
GongHedong <1470412156@qq.com>
2024-12-13 19:14:24 +0800

    feat: finish all

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'object-detection-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/app.js',
+    '/manifest.json',
+    'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs',
+    'https://cdn.jsdelivr.net/npm/@tensorflow-models/coco-ssd'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
